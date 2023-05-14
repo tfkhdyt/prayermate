d0189332c560b460276f4e046e30983aa8b395c9
tfkhdyt <tfkhdyt@proton.me>
2023-05-14 21:15:23 +0700

    refactor(show): combine some structs to single dto struct

--- pkg/api/show.go
+++ pkg/api/show.go
@@ -9,36 +9,30 @@ import (
 	"codeberg.org/tfkhdyt/prayermate/pkg/fetch"
 )
 
-type Coordinate struct {
-	Latitude     float64 `json:"lat"`
-	Longitude    float64 `json:"lon"`
-	LatitudeStr  string  `json:"lintang"`
-	LongitudeStr string  `json:"bujur"`
-}
-
-type Schedule struct {
-	Date   string `json:"tanggal"`
-	Imsak  string `json:"imsak"`
-	Subuh  string `json:"subuh"`
-	Terbit string `json:"terbit"`
-	Dhuha  string `json:"dhuha"`
-	Dzuhur string `json:"dzuhur"`
-	Ashar  string `json:"ashar"`
-	Magrib string `json:"magrib"`
-	Isya   string `json:"isya"`
-}
-
-type PrayerTimesData struct {
-	ID         string     `json:"id"`
-	Location   string     `json:"lokasi"`
-	Province   string     `json:"daerah"`
-	Coordinate Coordinate `json:"koordinat"`
-	Schedule   Schedule   `json:"jadwal"`
-}
-
 type ShowPrayerTimesDto struct {
-	Status bool            `json:"status"`
-	Data   PrayerTimesData `json:"data"`
+	Status bool `json:"status"`
+	Data   struct {
+		ID         string `json:"id"`
+		Location   string `json:"lokasi"`
+		Province   string `json:"daerah"`
+		Coordinate struct {
+			Latitude     float64 `json:"lat"`
+			Longitude    float64 `json:"lon"`
+			LatitudeStr  string  `json:"lintang"`
+			LongitudeStr string  `json:"bujur"`
+		} `json:"koordinat"`
+		Schedule struct {
+			Date   string `json:"tanggal"`
+			Imsak  string `json:"imsak"`
+			Subuh  string `json:"subuh"`
+			Terbit string `json:"terbit"`
+			Dhuha  string `json:"dhuha"`
+			Dzuhur string `json:"dzuhur"`
+			Ashar  string `json:"ashar"`
+			Magrib string `json:"magrib"`
+			Isya   string `json:"isya"`
+		} `json:"jadwal"`
+	} `json:"data"`
 }
 
 func ShowPrayerTimes(locationID string) (*ShowPrayerTimesDto, error) {
