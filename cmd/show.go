108d3c5152b28cb12543e441f361b24a5130d8d6
tfkhdyt <tfkhdyt@proton.me>
2023-05-15 09:07:49 +0700

    feat(show): read location id from config

--- cmd/show.go
+++ cmd/show.go
@@ -10,31 +10,22 @@ import (
 	"codeberg.org/tfkhdyt/prayermate/pkg/api"
 	"codeberg.org/tfkhdyt/prayermate/pkg/stdout"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // showCmd represents the show command
 var showCmd = &cobra.Command{
-	Use:   "show",
+	Use:   "show [location id...]",
 	Short: "Show prayer times for this day",
 	Long:  `Show prayer times for this day`,
-	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, locationID := range args {
-			prayerTimes, err := api.ShowPrayerTimes(locationID)
-			if err != nil {
-				log.Fatalf("Error: %v\n", err.Error())
+		if len(args) == 0 {
+			locationID := viper.GetString("locationId")
+			showSchedule(locationID)
+		} else {
+			for _, locationID := range args {
+				showSchedule(locationID)
 			}
-
-			fmt.Printf(`ID: %s
-Location: %s
-Province: %s
-Coordinate:
-  - Latitude: %s
-  - Longitude: %s
-Date: %s
-`, prayerTimes.Data.ID, prayerTimes.Data.Location, prayerTimes.Data.Province, prayerTimes.Data.Coordinate.LatitudeStr, prayerTimes.Data.Coordinate.LongitudeStr, prayerTimes.Data.Schedule.Date)
-
-			stdout.PrintScheduleTable(prayerTimes)
 		}
 	},
 }
@@ -51,4 +42,22 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// showCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func showSchedule(locationID string) {
+	prayerTimes, err := api.ShowPrayerTimes(locationID)
+	if err != nil {
+		log.Fatalf("Error: %v\n", err.Error())
+	}
+
+	fmt.Printf(`ID: %s
+Location: %s
+Province: %s
+Coordinate:
+  - Latitude: %s
+  - Longitude: %s
+Date: %s
+`, prayerTimes.Data.ID, prayerTimes.Data.Location, prayerTimes.Data.Province, prayerTimes.Data.Coordinate.LatitudeStr, prayerTimes.Data.Coordinate.LongitudeStr, prayerTimes.Data.Schedule.Date)
+
+	stdout.PrintScheduleTable(prayerTimes)
 }
