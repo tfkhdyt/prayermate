7154a694fda5394885dbcccac072de6e3da91487
tfkhdyt <tfkhdyt@proton.me>
2023-05-15 09:08:02 +0700

    feat: setup config

--- cmd/root.go
+++ cmd/root.go
@@ -5,9 +5,13 @@ package cmd
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -29,6 +33,7 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -38,4 +43,50 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.SetConfigName("config") // name of config file (without extension)
+		viper.SetConfigType("toml")   // REQUIRED if the config file does not have the extension in the name
+		viper.AddConfigPath(".")      // optionally look for config in the working directory
+		viper.AddConfigPath(filepath.Join(home, ".config", "prayermate"))
+		viper.SetDefault("locationId", "1301")
+	}
+
+	viper.ReadInConfig()
+
+	if _, err := os.Stat("$HOME/.config/prayermate/config.toml"); os.IsNotExist(err) {
+		createConfig()
+	}
+}
+
+func createConfig() {
+	// Create the directory and file paths.
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	dirPath := filepath.Join(home, ".config", "prayermate")
+	filePath := filepath.Join(dirPath, "config.toml")
+
+	// Create the directory if it does not exist.
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		os.MkdirAll(dirPath, 0755)
+	}
+
+	// Create the file if it does not exist.
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+	}
 }
