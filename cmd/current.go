3495fab1d74268f9be6642a0fcb031ae8a4dc13a
tfkhdyt <tfkhdyt@proton.me>
2023-05-16 10:12:19 +0700

    refactor!: rename location get command to location current

--- cmd/current.go
+++ cmd/current.go
@@ -11,9 +11,9 @@ import (
 	"github.com/spf13/viper"
 )
 
-// getCmd represents the get command
-var getCmd = &cobra.Command{
-	Use:   "get",
+// currentCmd represents the current command
+var currentCmd = &cobra.Command{
+	Use:   "current",
 	Short: "Show currently selected location",
 	Long:  `Show currently selected location`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -27,15 +27,15 @@ var getCmd = &cobra.Command{
 }
 
 func init() {
-	locationCmd.AddCommand(getCmd)
+	locationCmd.AddCommand(currentCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// currentCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// currentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
